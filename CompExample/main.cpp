f18ab00f995c7efaefd061cd9c351719ccefb87b
panqj <panqj01@mit.edu>
2024-04-13 20:04:54 -0400

    Initial Commit

---
+++ CompExample/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CompExample
+//
+//  Created by panqj on 4/13/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
